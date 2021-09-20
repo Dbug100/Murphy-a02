6029fb6a585ae9c5419eddff0c3992675b2fb7f7
deaja <deaja214@gmail.com>
2021-09-19 21:42:18 -0400

    Initial commit

---
+++ src/main/java/assignments/exercise/Solution08.java
@@ -0,0 +1,34 @@
+package assignments.exercise;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 8 Solution
+ *  Copyright 2021 Deaja Murphy
+ */
+
+
+/*
+ * Create String to store Name
+ * Print Statement asking for name
+ * Take user input into String name
+ * Print final output with the name of the User
+ */
+import java.util.Scanner;
+public class Solution08 {
+    public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
+        System.out.print("How many people? ");
+        int people = userIn.nextInt();
+        System.out.print("How many pizzas do you have? ");
+        int pizza = userIn.nextInt();
+        System.out.print("How many slices per pizza? ");
+        int slicesPer = userIn.nextInt();
+        int numSlices = slicesPer*pizza;
+        System.out.println(people +" people with " +pizza +" pizzas (" +numSlices +" slices)");
+        int perPerson = numSlices/people;
+        int remainder = numSlices % people;
+        System.out.println("Each person gets " +perPerson +" pieces of pizza.");
+        System.out.print("There are " +remainder +" leftover pieces.");
+
+
+
+    }
+}
